2151be35a5a2b51dd9f9795b600a516b4d947f77
solidcc2 <solidcc@qq.com>
2021-12-17 04:02:33 -0500

    update: gpumemwatch.py

--- python/gpumemwatch.py
+++ python/gpumemwatch.py
@@ -1,10 +1,13 @@
 
 from multiprocessing import Process
+from matplotlib.pyplot import figure
+from numpy import record
 import pynvml as nvml
 from enum import Enum
 import time
 import json
 import signal
+
 
 from p2pconnect import P2PServer
 from delaymicrosecond import delayMicrosecond
@@ -35,7 +38,7 @@ class GPUMemWatch:
         print(f"\n\rGPU Device list:")
         for i in range(self._deviceNum):
             devHandle = nvml.nvmlDeviceGetHandleByIndex(i)
-            devName = str(nvml.nvmlDeviceGetName(devHandle))
+            devName = str(nvml.nvmlDeviceGetName(devHandle), encoding="utf8")
             print(f"\n\r\t{i}/{self._deviceNum}\t{devName}")
 
     def setDevice(self, devID):
@@ -44,6 +47,10 @@ class GPUMemWatch:
     def getGPUMem(self):
         memSummary = nvml.nvmlDeviceGetMemoryInfo(self._devHandle)
         return memSummary.free, memSummary.used, memSummary.total
+
+    def getGPUUtilize(self):
+        utilizeSummary = nvml.nvmlDeviceGetUtilizationRates(self._devHandle)
+        return utilizeSummary.gpu
 
     def save(self):
         with open(self._outfile, "w") as f:
@@ -72,11 +79,36 @@ class GPUMemWatch:
                 break
 
             if self._status is STATUS.WATCHING:
-                self.record[time.time()] = self.getGPUMem() # free, used, total
+                t = time.time()
+                self.record[t] = {}
+                self.record[t]["mem"] = self.getGPUMem()    # free, used, total
+                self.record[t]["utilize"] = self.getGPUUtilize()    # gpu utilize
             delayMicrosecond(self._interval)
 
         self._conn.close()
         self.save()
+
+    # TODO: 画图横坐标单位
+    def drawUsed(self, filename="used.jpg"):
+        from matplotlib import pyplot as plt
+        import numpy as np
+        x = [key * 10000 for key in self.record.keys()]     # 换为100微秒为单位
+        y = list(map(lambda x: x["mem"][1], self.record.values()))
+        fig = plt.figure()
+        plt.plot(x, y)
+        plt.savefig(filename)
+
+    # TODO: 画图横坐标单位
+    def drawUtilize(self, filename="utilized.jpg"):
+        from matplotlib import pyplot as plt
+        import numpy as np
+        x = [key * 10000 for key in self.record.keys()]
+        tmp = x[0]
+        x = [k - tmp for k in x]
+        y = list(map(lambda x: x["utilize"], self.record.values()))
+        fig = plt.figure()
+        plt.plot(x, y)
+        plt.savefig(filename)
 
 def testClient():
     from p2pconnect import P2PClient
@@ -97,6 +129,8 @@ if __name__=="__main__":
         gpuwatch = GPUMemWatch(("127.0.0.1", 3344))
         gpuwatch.printGPUsInfo()
         gpuwatch.watch(0)
+        gpuwatch.drawUsed()
+        gpuwatch.drawUtilize()
     elif args.role == "client":
         testClient()
     else:
